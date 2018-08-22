2cb87b4387962fd1c98859a6f371cd3da192c213
kiattikunchobtham <42606491+kiattikunchobtham@users.noreply.github.com>
2018-08-22 17:39:43 +0700

    Add files via upload

---
+++ Cann.py
@@ -0,0 +1,28 @@
+"1 Excel"
+import csv
+ifile = open('test.csv', "r")
+reader = csv.reader(ifile)
+ofile = open('ttest2.csv', "wb")
+writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+for row in reader:
+    print(row)
+
+
+
+ifile.close()
+ofile.close()
+
+"2 variable"
+target_color_name='FireBrick'
+target_color_rgb= (178,34,34)
+target_color_name=first_color_name='FireBrick'
+
+print(id(target_color_name)==id(first_color_name))
+
+total_count=0
+total_count+=5
+total_count+=6
+print(total_count)
+
+import git
